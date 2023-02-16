61887dadf52026ca595e7a5035ae7cf725a75c00
Hans Baier <hansfbaier@gmail.com>
2023-02-16 09:40:56 +0700

    run-gui: add USB capability

--- run-gui.sh
+++ run-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost +
-time docker run -it --rm -v $(pwd):/build  --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" raetro/quartus:17.0 quartus *.qpf
+time docker run -it --rm -v $(pwd):/build  --net=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb --privileged --env="QT_X11_NO_MITSHM=1" raetro/quartus:17.0 quartus *.qpf
