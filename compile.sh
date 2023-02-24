244eb897803bf3a78f365045b5f2c26a2d63fbd5
Hans Baier <hansfbaier@gmail.com>
2023-02-25 04:46:37 +0700

    compile.sh: generate rbf automatically

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+test -d output_files || mkdir output_files
 quartus_sh --flow compile *.qpf
+SOF=$(echo output_files/*.sof)
+RBF=$(echo $SOF | sed s,sof,rbf,g)
+quartus_cpf -c $SOF $RBF
+
