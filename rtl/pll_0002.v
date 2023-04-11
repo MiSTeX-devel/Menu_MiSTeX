17e830bf2158c3866e617629d8437d26d4c79878
Hans Baier <hansfbaier@gmail.com>
2023-04-12 06:20:55 +0700

    use Template sys and make it work on the terasic DECA board

--- rtl/pll_0002.v
+++ rtl/pll_0002.v
@@ -1,6 +1,5 @@
 `timescale 1ns/10ps
 module  pll_0002(
-
 	input wire refclk,
 	input wire rst,
 	output wire outclk_0,
@@ -8,6 +7,7 @@ module  pll_0002(
 	output wire locked
 );
 
+`ifdef CYCLONEV
 	altera_pll #(
 		.fractional_vco_multiplier("false"),
 		.reference_clock_frequency("50.0 MHz"),
@@ -29,5 +29,31 @@ module  pll_0002(
 		.fbclk	(1'b0),
 		.refclk	(refclk)
 	);
+`else
+	ALTPLL #(
+		.BANDWIDTH_TYPE("AUTO"),
+		.CLK0_DIVIDE_BY(10'd15),
+		.CLK0_DUTY_CYCLE(6'd50),
+		.CLK0_MULTIPLY_BY(10'd30), // 100MHz = 50MHz * 30 / 15
+		.CLK0_PHASE_SHIFT(1'd0),
+		.CLK1_DIVIDE_BY(10'd75),
+		.CLK1_DUTY_CYCLE(6'd50),
+		.CLK1_MULTIPLY_BY(10'd30), // 20MHz = 50MHz * 30 / 75
+		.CLK1_PHASE_SHIFT(1'd0),
+		.COMPENSATE_CLOCK("CLK0"),
+		.INCLK0_INPUT_FREQUENCY(15'd20000),
+		.OPERATION_MODE("NORMAL")
+	) altera_pll_i (
+		.ARESET(1'd0),
+		.CLKENA(5'd31),
+		.EXTCLKENA(4'd15),
+		.FBIN(1'd1),
+		.INCLK(refclk),
+		.PFDENA(1'd1),
+		.PLLENA(1'd1),
+		.CLK({outclk_1, outclk_0}),
+		.LOCKED(locked)
+	);
+`endif
 endmodule
 
